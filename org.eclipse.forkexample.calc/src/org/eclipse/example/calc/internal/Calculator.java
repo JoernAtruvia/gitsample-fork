135fd7c1ad0884e1e4181aa591bcbaeefe542b44
Mark Niehues <Mark.Niehues@atruvia.de>
2022-05-11 11:45:36 +0200

    new comment in master

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -6,7 +6,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * WorkingCopy in NIU
+ * WorkingCopy in NIU new on master
  *******************************************************************************/
 package org.eclipse.example.calc.internal;
 
